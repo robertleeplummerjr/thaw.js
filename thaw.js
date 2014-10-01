c2fd7b8d233e9aef2e828d2514840e6b33a30f52
robertleeplummerjr <Adminuser1>
2014-10-01 10:09:03 -0400

    Better naming, lookup, and added thawing to done.

--- thaw.js
+++ thaw.js
@@ -1,6 +1,6 @@
 var thaw = (function() {
-	function createFutureDate(milliseconds)
-	{
+
+	function createFutureDate(milliseconds) {
 		var t = new Date();
 		t.setTime(t.getTime() + milliseconds);
 		return t;
@@ -12,26 +12,14 @@ var thaw = (function() {
 		},
 		thawing = false;
 
-	function thaw(items, burnTime, options) {
-		var i = 0,
-			_each = function() {
-				if(i<0) return;
-
-				setTimeout(_each, 0);
-				if (!thawing) {
-					if ((new Date()) < createFutureDate(burnTime)) {
-						if (i >= items.length) {
-							i = -1;
-							options.done.call(items[i]);
-							return;
-						}
-						thawing = true;
-						options.each.call(items[i], i);
-						thawing = false;
-						i++;
-					}
-				}
-			};
+	/**
+	 *
+	 * @param {Array} items
+	 * @param {Object} [options]
+	 * @param {Number} [burnTime]
+	 */
+	function thaw(items, options, burnTime) {
+		burnTime = (burnTime !== undefined ? burnTime : 50);
 
 		if (options !== undefined) {
 			for(var key in defaultSettings) {
@@ -41,9 +29,36 @@ var thaw = (function() {
 					}
 				}
 			}
+		} else {
+			options = {};
 		}
 
-		_each();
+		var i = 0,
+			each = (options.each !== undefined ? options.each : null),
+			done = (options.done !== undefined ? options.done : null),
+			tick = function() {
+				if(i<0) return;
+
+				setTimeout(tick, 0);
+
+				if (!thawing) {
+					if ((new Date()) < createFutureDate(burnTime)) {
+						if (i >= items.length) {
+							i = -1;
+							thawing = true;
+							if (done !== null) done.call(items[i]);
+							thawing = false;
+							return;
+						}
+						thawing = true;
+						if (each !== null) each.call(items[i], i);
+						thawing = false;
+						i++;
+					}
+				}
+			};
+
+		tick();
 	}
 
 	thaw.isThawing = function() {
